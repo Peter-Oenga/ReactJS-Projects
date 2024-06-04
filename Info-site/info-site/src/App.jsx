3eef994a649c3ca46ceafa2412a047adc6ec19f9
Peter-Oenga <poenga230@gmail.com>
2024-06-05 00:02:09 +0300

    Music application

---
+++ Info-site/info-site/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <Main /> */}
+      <Navbar />
+    </>
+  )
+}
+
+export default App;
---
+++ Info-site/info-site/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const>
+<h2>I hope this works</h2> = () => {
+  return (
+    <div>
+        <h2>I hope this works</h2>
+    </div>
+  )
+}
